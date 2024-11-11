5ef7d97762b7f9102df741ff61f54942d944ca28
srikar <srikarrao2021@gmail.com>
2024-11-11 15:01:54 +0530

    Completed program1 and program2

--- program1.py
+++ program1.py
@@ -1,10 +1,29 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        def opposite(s):
+            if s==')':
+                return '('
+            if s=='}':
+                return '{'
+            return '['
+        stack , n = [] , 0
+        for i in s:
+            if i=='(' or i=='{' or i=='[':
+                stack.append(i)
+                n+=1
+            else:
+                if n>0:
+                    if stack[n-1] == opposite(i):
+                        stack.pop()
+                        n-=1
+                    else:
+                        break
+                else:
+                    n = 1
+                    break
+        if n!=0:
+            return False
+        return True
 
 
 
